3a7203bbe8f3bf055287c35f71d951236a99e450
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-04-23 11:39:08 +0200

    Add lazy load with IntersectionObserver

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('We are in!');
+
+const images = document.querySelectorAll('img');
+
+let imageOptions = {};
+
+let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) return;
+        const image = entry.target;
+        image.src = image.src.replace('&w=10&', '&w=800&');
+        observer.unobserve(image);
+    }, imageOptions);
+});
+
+images.forEach((image) => {
+    observer.observe(image);
+});
